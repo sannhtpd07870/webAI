77fd229533a2d04eee68f42c832856063bee0191
SanNHT <nguyen.h.thaisan@gmail.com>
2024-06-13 08:47:33 +0700

    udate 8:47 navigation

--- src/index.js
+++ src/index.js
@@ -33,11 +33,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      <Route path="/GiaTri" element={<Corevalues />} />
     <Route path="/DoiTac" element={<Partn/>} />
     <Route path="/News" element={<News/>} />
-
     <Route path="/SanPham/Solution" element={<K_SOLUTION />}/>
     <Route path="/SanPham/Healthtech" element={<K_HEALTHTECH />} />
     <Route path="/SanPham/Technical" element={<K_TECHNICAL />}/>
-    {/* <Route path="/Admin" element={<Admin />} /> */}
   </Routes>
   <Footer className="footer"/>
 </Router> 
